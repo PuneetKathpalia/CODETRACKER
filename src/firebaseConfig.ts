ebcb6a6e5c61757231b2fc29d5f3113d1e67769e
Puneet Kathpalia <puneetkathpalia99@gmail.com>
2025-05-14 18:11:11 +0530

    niw

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYLCWLv2a_L-C6zmI_-PVPQv1hHWkj2TI",
+  apiKey: "AIzaSyDYLCWLu2a_L-C6zmI_-PVPQVhlHWkj2II",
   authDomain: "codetracker-46cd5.firebaseapp.com",
   projectId: "codetracker-46cd5",
   storageBucket: "codetracker-46cd5.appspot.com",
   messagingSenderId: "82398464229",
-  appId: "1:82398464229:web:4a3c003b7ee52280edec0c",
-  measurementId: "G-B7N1L4YDHS"
+  appId: "1:82398464229:web:82236d57fc484bf3edec0c",
+  measurementId: "G-3RPX8W3GSM"
 };
 
 const app = initializeApp(firebaseConfig);
